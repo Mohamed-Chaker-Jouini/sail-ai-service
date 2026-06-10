7be1ca8255277a8af183dff75986c7b1f24e310b
Mohamed Chaker Jouini <mohamed-chaker.jouini@hpe.com>
2026-06-10 16:14:36 +0100

    Go back to docker

---
+++ nvidia_container_toolkit.sh
@@ -0,0 +1,13 @@
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.p/nvidia-container-toolkit.list
+
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+sudo nvidia-container-toolkit runtime configure --runtime=docker
+
+sudo systemctl restart docker
+
+docker run --rm --gpus all nvidia/cuda:12.0.0-base-ubuntu22.04 nvidia-smi
